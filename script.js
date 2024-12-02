f2d61acd53cf0be7f731538848be22be86757364
Ayush Dev <ayushdev.in@gmail.com>
2024-12-02 19:59:54 +0530

    added new game button and removed errors

--- script.js
+++ script.js
@@ -11,9 +11,13 @@ function updateTrophy() {
     if (hmscore > awscore) {
         const el = document.getElementById('home-team');
         el.querySelector('h3').textContent = 'Home🏆';
+        const el2 = document.getElementById('away-team');
+        el2.querySelector('h3').textContent = 'Away';
     } else if (hmscore < awscore) {
         const el = document.getElementById('away-team');
         el.querySelector('h3').textContent = 'Away🏆';
+        el2 = document.getElementById('home-team');
+        el2.querySelector('h3').textContent = 'Home';
     } else {
         document.getElementById('home-team').querySelector('h3').textContent = 'Home';
         document.getElementById('away-team').querySelector('h3').textContent = 'Away';
@@ -56,3 +60,10 @@ function home_three() {
     updateTrophy();
 }
 
+function new_game() {
+    hmscoreEl.textContent = 0;
+    awscoreEl.textContent = 0;
+    hmscore = 0;   
+    awscore = 0;
+    updateTrophy();
+}
